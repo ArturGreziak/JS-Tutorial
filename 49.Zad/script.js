ed6a8dc7161eb849346ca07d0cfe38028261d5de
Artur Gręziak <alex4448@wp.pl>
2022-01-13 16:36:09 +0100

    splice method

---
+++ 49.Zad/script.js
@@ -0,0 +1,12 @@
+// Metoda fill
+
+const tab = [0, 1, 2, 3, 4];
+
+tab.fill(7);
+console.log("tab: ", tab);
+
+tab.fill(5, 2);
+console.log("tab: ", tab);
+
+tab.fill(6, 1, 4);
+console.log("tab: ", tab);
