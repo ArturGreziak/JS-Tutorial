18aea32871facb2a54b0306bb6b50d4ba116b46e
Artur Gręziak <alex4448@wp.pl>
2022-01-13 15:18:06 +0100

    while loop

---
+++ 26.Zad/script.js
@@ -0,0 +1,16 @@
+// Loop while
+/*
+while(expression){
+    statement
+}
+
+while(true){
+    console.log("to się nigdy nie kończy");
+}
+*/
+let count = 0;
+
+while(count < 9){
+    console.log("count: ", count);
+    count++;
+}
