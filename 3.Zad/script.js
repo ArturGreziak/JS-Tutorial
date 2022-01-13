8679638fb3a24fe0e057b3eca6062e141e638e81
Artur Gręziak <alex4448@wp.pl>
2022-01-13 12:14:31 +0100

    reserved words and semicolons identifiers

---
+++ 3.Zad/script.js
@@ -0,0 +1,66 @@
+// Identyfikatory
+
+/*
+m
+_my
+my_variable
+$str
+v2
+*/
+/*
+abstract (*)	as (*)	boolean
+break	byte	case
+catch	char	class (*)
+continue	const (*)	debugger (*)
+default	delete	do
+double	else	enum (*)
+export (*)	extends (*)	false
+final	finally	float
+for	function	goto (*)
+if	implements (*)	import (*)
+in	instanceof	int
+interface (*)	is (*)	long
+namespace (*)	native (*)	new
+null	package (*)	private (*)
+protected (*)	public (*)	return
+short	static (*)	super (*)
+switch	synchronized (*)	this
+throw	throws (*)	transient (*)
+true	try	typeof
+use (*)	var	void
+volatile (*)	while	with
+*/
+
+// Średniki ;
+// Nie wymagane średniki:
+// let a = 2
+let b = 4
+
+let a 
+a
+=
+1
+console.log("a:", a);
+// Wymagane średniki:
+// let c = 6; let d = 8;
+
+let c; c = 6; 
+console.log("c:", c);
+
+let e = 0
+;[e, e+1].forEach(console.log)
+
+/*
+return
+true;
+*/
+
+/*
+przypuszczenie
+return; true;
+*/
+
+/*
+zamiar
+return true;
+*/
