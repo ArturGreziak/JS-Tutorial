921d444ea45da552f898aec17c210a236d372990
Artur Gręziak <alex4448@wp.pl>
2022-01-13 16:02:15 +0100

    array length

---
+++ 36.Zad/script.js
@@ -0,0 +1,23 @@
+// Długość talic 
+
+let tab = []; // []
+
+console.log(tab.length); // 0
+
+let tab2 = [7, 7, 7]; // [7, 7, 7]
+
+console.log(tab2.length); // 3
+
+let tab3 = [0, 1, 2, 3, 4, 5, 6]; // 7
+
+tab3.length = 5;
+
+console.log(tab3);
+
+tab3.length = 0;
+
+console.log(tab3);
+
+tab3.length = 4;
+
+console.log(tab3);
