1db0ca10158640b5588421fcbb654c7188e01442
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:34:40 +0100

    equality and inequality operators

---
+++ 19.Zad/script.js
@@ -0,0 +1,42 @@
+// Operatory równości i nie nie rowności
+/*
+= // is assiigned
+
+== // is equal to
+
+=== // is strictly equal to
+
+!= // not equal to
+
+!== // not strictly equal to
+*/
+
+1 === "1"; // false
+
+null === null // true
+
+undefined === undefined // true
+
+null === undefined // false
+
+NaN === NaN // false
+
+7 === 7 // true
+
+0 === -0 // true
+
+"hello" === "hello" // true
+"hello" === "Hello" // false
+
+null == null // true
+NaN == NaN // false
+
+null == undefined // true
+
+7 == "7" // true
+7 == "6" // false
+
+treu == 1 // true
+false == 0 // true
+
+"0" == false // true
