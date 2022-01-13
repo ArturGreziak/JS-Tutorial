397c6a3b8fd2970e7212f809da8327acb07b5b62
Artur Gręziak <alex4448@wp.pl>
2022-01-13 15:39:34 +0100

    arrays

---
+++ 30.Zad/script.js
@@ -0,0 +1,12 @@
+// Arrays
+
+// Uporządkowany zbiór wartości
+
+// Indeksowanie od 0 do 4,294,967,294 (2**32 - 2)
+
+// Maksymalny rozmiar 4,294,967,295
+
+// Właściwość length
+
+const tab = ["Artur", 4, true];
+
