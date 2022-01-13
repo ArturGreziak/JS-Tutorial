4671d9d85770ab6abf93fedeb85199a9f316cc90
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:25:47 +0100

    declaration of variables and assignment

---
+++ 15.Zad/script.js
@@ -0,0 +1,45 @@
+// Deklarowanie zmiennych
+/*
+let m;
+let name;
+
+let m, name;
+
+let firstName = "Artur";
+let a = 4, b = a * a;
+
+const Y = 0.5772156649; // stała Eulera
+
+const NUMBER_OF_COLUMNS = 4;
+
+for(let m = 0; m < n; m++){
+    console.log(m);
+}
+
+for(const property in object){
+    console.log(property);
+*/
+
+/*
+const m = 1;
+
+if (m === 1){
+    let m = 5;
+    m++;
+    console.log("m: " + m); // 6
+}
+console.log("m: " + m); // 1
+*/
+
+/*
+const m = 1;
+
+if (m === 1){
+    let m = 5;
+    m++;
+    console.log("m: " + m); // 6
+}
+console.log("m: " + m); // 1
+
+let m = 2;  // SyntaxError
+*/
