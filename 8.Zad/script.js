69724e8dbdb3c265de138d382cc763b4833dbe4a
Artur Gręziak <alex4448@wp.pl>
2022-01-13 12:57:27 +0100

    Rounding errors

---
+++ 8.Zad/script.js
@@ -0,0 +1,25 @@
+// Błędy zaokrągleń 
+
+/*
+64 bit floating-point
+IEEE 754 standard 
+
+
+18,437,736,874,454,810,627
+
+1/2, 1/8, 1/1024
+
+1/10, 1/100
+
+0.3 === .3 // true
+
+let a = .3 - .2;
+let b = .2 - .1;
+
+a === b // false
+
+console.log("a", a); // 0.09999999999999998
+
+console.log("b", b); // 0.1 
+
+*/
