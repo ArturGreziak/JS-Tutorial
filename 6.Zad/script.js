f9b4ca103eed9e93a8d068195b6e8a3e0ca98bf7
Artur Gręziak <alex4448@wp.pl>
2022-01-13 12:40:05 +0100

    boolean type

---
+++ 6.Zad/script.js
@@ -0,0 +1,36 @@
+console.log(typeof(true)); // Boolean
+console.log(typeof(false)); // Boolean
+
+let a = 17;
+
+
+a === 17 // false
+
+if(a === 18){
+    console.log("true");
+}else if(a === 17){
+    console.log("false");
+}
+
+// Falsy Value
+
+/*
+undefined
+null
+0
+-0
+NaN
+""
+*/
+
+// Operatory logiczne 
+
+/*
+
+&& logiczne and wtedy i tylko wtedy kiedy oba warunki są prawdziwe;
+
+|| logiczne or wtegy kiedy jedna z wartości jest prawdą
+
+! wartość not neguje wartości
+
+*/
