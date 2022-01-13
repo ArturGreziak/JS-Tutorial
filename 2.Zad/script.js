9b69f1d7e772fe4efa398a1debecc4899e48e340
Artur Gręziak <alex4448@wp.pl>
2022-01-13 12:02:20 +0100

    development tools

---
+++ 2.Zad/script.js
@@ -0,0 +1,27 @@
+// let a = hello;
+
+// let b = Hello;
+
+// let c = HELLO; 
+
+// komentarz
+
+/*
+komentarz
+komentarz
+*/
+/*
+let d = 54;
+
+let e = 2.9;
+
+let f = "Hello Word!";
+
+let g = 'hello';
+
+let h = true;
+
+let i = false;
+
+let j = null;
+*/
