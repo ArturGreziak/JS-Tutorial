767884586bccd9c52228f8dac57507a56af06319
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:23:24 +0100

    Implicit Type Conversion

---
+++ 14.Zad/plik.md
@@ -0,0 +1,3 @@
+# Nie jawna konwersjia typów
+
+![enter image description here](https://cdn.discordapp.com/attachments/674964991853592586/930195146555924530/konwersja.png)
---
+++ 14.Zad/script.js
@@ -0,0 +1,23 @@
+let result;
+
+result = 1 + "2";
+console.log(result);
+typeof(result); // string
+
+result = "1" + null;
+console.log(result);
+typeof(result); // string
+
+result = "7" * "7";
+console.log(result);
+typeof(result); // number
+
+result = "7" - "2";
+console.log(result);
+typeof(result); // number
+
+result = 1 - "m";
+console.log(result);
+typeof(result); //number
+ 
+   
