37958514d1dd980035b97ad4059a31065e460594
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:38:16 +0100

    comparison operators

---
+++ 20.Zad/script.js
@@ -0,0 +1,19 @@
+//Operatory porównania
+
+let a = 3;
+let b = 2;
+
+console.log(a > b); // true
+console.log(a < b); // false
+
+let c = 3;
+let d = 2;
+
+console.log(c >= d); // true
+console.log(c <= d); // false
+
+/*
+Argumenty dowolnego typu
+Porównywania mogą być wykonywane tylko na 
+liczbach i ciągach znaków
+*/
