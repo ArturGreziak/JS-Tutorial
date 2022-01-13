96be11431e60abcba24d1f0c33b8bcea8c86555f
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:59:05 +0100

    conditional if else and switch statements

---
+++ 25.Zad/script.js
@@ -0,0 +1,45 @@
+// Instrukcje warunkowe
+
+// let m = 3;
+
+// if(m === 1){
+//     console.log("nice");
+// }else if(m === 2){
+//     console.log("good");
+// }else if(m === 3){
+//     console.log("very good");
+// }else {
+//     console.log("okay");
+// }
+
+// switch(expression){
+//     case x:
+//         statement_1;
+//         break;
+//     case y:
+//         statement_2;
+//         break;
+//     case z:
+//         statement_3;
+//         break;
+//     default:
+//         statement_4;
+//         break; 
+// }
+
+let a = 3;
+
+switch (a) {
+    case 1:
+        console.log("nice");
+        break;
+    case 2:
+        console.log("good");
+        break;
+    case 3:
+        console.log("very good");
+        break;
+    default:
+        console.log("okay");
+        break;
+}
