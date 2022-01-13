e54370ac98428de63b6ed5faa914ed6cca1342bf
Artur Gręziak <alex4448@wp.pl>
2022-01-13 16:25:39 +0100

    concat method

---
+++ 44.Zad/script.js
@@ -0,0 +1,13 @@
+// Metoda concat()
+
+const tab = [7, 6, 5];
+
+const tab2  = tab.concat(1,2);
+
+console.log(tab2);
+
+const tab3 = tab.concat([1,2, [3,4]]);
+
+console.log(tab3);
+
+console.log("tab: ", tab);
