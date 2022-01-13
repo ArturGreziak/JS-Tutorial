219a0ba9d73772a5f2873c89c1bc35adc38b807e
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:28:49 +0100

    declaration of variables and assignment of var

---
+++ 16.Zad/script.js
@@ -0,0 +1,51 @@
+/*
+var name = "Artur";
+
+var m = 5, x = m * m;
+console.log(x);
+*/
+
+/*
+var m = 5;
+
+if(m === 5){
+    console.log("m: ", m); //5
+
+    var m = 10;
+
+    console.log("m: ", m); //10
+
+    m++;
+
+    console.log("m: ", m); //11
+}
+    console.log("m: ", m); //11
+
+*/
+
+/*
+var m = 7;
+
+globalThis.m = 7;
+*/
+
+/*
+var data = 5;
+
+for(var i = 0; i < data; i++){
+    console.log("i: ", i);
+}
+*/
+
+/*
+m = 7;
+console.log("m: ", m); //7
+
+var m;
+
+console.log("m: ", m ); //7
+
+var m;
+
+m = 7;
+*/
