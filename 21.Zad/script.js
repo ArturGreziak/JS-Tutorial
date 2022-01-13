d4d56128985e07e9be29fde397dc0613c9151f25
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:40:16 +0100

    operatory logiczne

---
+++ 21.Zad/script.js
@@ -0,0 +1,36 @@
+// Operatory logiczne
+
+let a = 1;
+let b = 2;
+
+a === 1 && b === 2 // true
+
+a === 1 && b === 3 // false
+
+a === 3 && b === 2 // false
+
+a === 3 && b === 3 // false
+
+a === 1 || b === 2 // true
+
+a === 1 || b === 3 // true
+
+a === 3 || b === 2 // true
+
+a === 3 || b === 3 // false
+
+let m = true;
+
+let n = false;
+
+!m; // false
+
+!n; // true
+
+let f = "hello";
+
+let p = "";
+
+!!f  // true
+
+!!p   // false
