5203a6c8fd8e453593185f2a1b71acefae4b54a8
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:49:35 +0100

    conditional operator

---
+++ 22.Zad/script.js
@@ -0,0 +1,15 @@
+// Conditional Operator
+
+// Ternary operator
+let n = -7;
+console.log(n > 0 ? n : -n); // 7
+
+// let m = -5;
+// console.log(m > 0 ? m : -m); // 5
+
+// let a = -7;
+// if(m > 0){
+//     console.log(m);
+// } else {
+//     console.log(-m)
+// }
