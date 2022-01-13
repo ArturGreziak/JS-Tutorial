2068fc9f097e0f9f6254619f10ea27ef4c03bd36
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:30:17 +0100

    dostęp do właściwości

---
+++ 17.Zad/script.js
@@ -0,0 +1,32 @@
+// Dostęp do wlaściwości do Object
+
+//experession.indentifier
+
+//experession[experession]
+/*
+let obj = { a: 1, b: { c: 2} };
+
+
+console.log(obj.a); // 1
+console.log(obj.b.c); // 2
+obj["a"] // 1
+obj["b"]["c"] // 2
+
+let obj = { a: 1, b: { c: 2 } };
+let arr = [ obj, 2, [3, 4]];
+
+arr[0];  // {a: 1, b: {}}
+arr[0].a  // 1
+arr[1]    // 3
+arr[2]    // [4, 5]
+arr[2]["1"]  // [5]
+arr["2"]["2"]  // [5]
+*/
+
+/*
+let  obj = {a: 1, b: 2};
+
+obj.c  // undefined
+
+obj["c"]; // undefined
+*/
