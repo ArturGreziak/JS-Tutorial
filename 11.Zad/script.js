965f31f8ff4ce2f1422e184857118e3ef704824c
Artur Gręziak <alex4448@wp.pl>
2022-01-13 14:10:05 +0100

    template literals

---
+++ 11.Zad/script.js
@@ -0,0 +1,19 @@
+// Templejt literal 
+
+let name = "Artur";
+
+let hello = `Hello ${name}`;
+
+console.log(hello);
+
+let m = `\home`;
+
+console.log(m.length);
+
+console.log(m);
+
+let n = String.raw`\home`;
+
+console.log(n);
+
+console.log(n.length);
