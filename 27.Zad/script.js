dc7cb9ba6c6c84207d6e130e43d4908e6e097d6d
Artur Gręziak <alex4448@wp.pl>
2022-01-13 15:24:24 +0100

    do while loop

---
+++ 27.Zad/script.js
@@ -0,0 +1,15 @@
+// Pętla do while
+
+let count = 0;
+
+do{
+    console.log("count: ", count);
+    count++;
+}while(count < 9);
+
+let counts = 9;
+
+do{
+    console.log("counts: ", counts);
+    counts++;
+}while(counts < 9);
