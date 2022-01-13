5b814c37fd1ddca6ea12246125683f49ef158c19
Artur Gręziak <alex4448@wp.pl>
2022-01-13 15:55:56 +0100

    creating an array using a constructor

---
+++ 34.Zad/script.js
@@ -0,0 +1,13 @@
+// Teh Arrays Constructor
+
+// let tab = new Array();
+
+// console.log(tab);
+
+let tab = new Array(8);
+
+console.log(tab);
+
+let tab2 = new Array(1, 2, 3, "A", "b", "c");
+
+console.log(tab2);
