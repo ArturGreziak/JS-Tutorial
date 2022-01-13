859a99b208630236797f1e0176bf5357f48784c3
Artur Gręziak <alex4448@wp.pl>
2022-01-13 16:07:18 +0100

    filter method

---
+++ 39.Zad/script.js
@@ -0,0 +1,15 @@
+// Array Iterator Method filter
+
+const age = [18, 17, 21, 16, 25, 24];
+
+let filterEd = age.filter(value => value >= 18);
+
+console.log(age);
+
+console.log(filterEd);
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
